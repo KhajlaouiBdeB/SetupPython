b0969aa88a11df5238be9121097a04b6c7fdcb59
Khalil <khalil.hajlaoui@bdeb.qc.ca>
2022-08-25 23:08:11 -0400

    [Setup] install cmake script

--- Install_cmake.py
+++ Install_cmake.py
@@ -5,12 +5,12 @@ import shutil
 import subprocess
 
 print("Start install")
-package = 'scikit-build'
-dir_path = './Scikit'
+package = 'cmake'
+dir_path = './Cmake'
 if os.path.exists(dir_path):
     print('remove ' + dir_path)
     shutil.rmtree(dir_path)
 
 print('Install ' + package)
 sys.stdout.flush() # always flush stream before subprocess call
-subprocess.check_call([sys.executable, '-m', 'pip', 'install', package, '--target', 'Scikit'])
+subprocess.check_call([sys.executable, '-m', 'pip', 'install', package, '--target', dir_path])
---
+++ Install_cmake.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+./Python310/python Install_cmake.py
+
+read -rsp $'Press any key to continue...\n' -n1 key
+
